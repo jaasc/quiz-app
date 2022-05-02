bf617f8e9a44fdb5a9972f3a0c818649bb7a1811
Muning <muning@Munings-MacBook-Pro.local>
2022-05-02 13:18:45 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 export default function App(){
     return(
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/questions" element={<Questions/>}/>
