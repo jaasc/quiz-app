58ab958e0c31a7260c7272828d0383d40ff5c3c9
Muning <muning@Munings-MacBook-Pro.local>
2022-05-02 13:23:36 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Questions from "./components/Questions"
 import Home from "./components/Home"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 
 export default function App(){
     return(
